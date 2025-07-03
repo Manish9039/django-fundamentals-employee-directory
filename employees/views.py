bba7ce0fae7f8e1495a11c55e9b73498e0318286
Manish Kumar Singh <msingh27503@gmail.com>
2025-07-03 09:09:20 +0530

    Initial Commit with .gitignore

---
+++ employees/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from employees.models import Employee
+
+
+# Create your views here.
+
+def employee_detail(request, pk):
+    # try:
+    #     employee = Employee.objects.get(pk=pk)
+    # except:
+    #     raise Http404
+    employee = get_object_or_404(Employee, pk=pk)
+    context = {
+        'employee': employee,
+    }
+    return render(request, 'employee_detail.html', context)
